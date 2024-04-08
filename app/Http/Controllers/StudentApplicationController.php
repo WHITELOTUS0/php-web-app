92e3a5fd281946fe842a61353f6307278622ef0c
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2024-04-08 12:57:05 +0300

    student application model

---
+++ app/Http/Controllers/StudentApplicationController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\StudentApplication;
+use Illuminate\Http\Request;
+
+class StudentApplicationController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(StudentApplication $studentApplication)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(StudentApplication $studentApplication)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, StudentApplication $studentApplication)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(StudentApplication $studentApplication)
+    {
+        //
+    }
+}
